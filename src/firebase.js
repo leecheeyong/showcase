9d9a3331ac1385270eaf3af13e6a03b5a2e666c2
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2025-07-17 02:43:35 +0800

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "project-showcase-b60de.firebaseapp.com",
+  projectId: "project-showcase-b60de",
+  storageBucket: "project-showcase-b60de.firebasestorage.app",
+  messagingSenderId: "265517201692",
+  appId: "1:265517201692:web:ac8c8b1c3c80466fc8e2d9"
+};
+
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const auth = getAuth(app)
+const storage = getStorage(app)
+
+export { db, auth, storage }
